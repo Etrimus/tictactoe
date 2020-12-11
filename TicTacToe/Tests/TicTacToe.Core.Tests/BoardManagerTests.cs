ce7eaa4b893d646ed031fa5d2c38985555bea357
Artem <artem@artem>
2020-12-11 15:02:30 +0500

    24

--- TicTacToe/Tests/TicTacToe.Core.Tests/BoardManagerTests.cs
+++ TicTacToe/Tests/TicTacToe.Core.Tests/BoardManagerTests.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Linq;
 using AutoFixture;
 using FluentAssertions;
-using FluentAssertions.Execution;
-using TicTacToe.Core.Models;
 using TicTacToe.Core.Services;
 using Xunit;
 
@@ -16,54 +13,9 @@ namespace TicTacToe.Core.Tests
         private readonly BoardManager _boardManager = new BoardManager();
 
         [Fact]
-        public void CreateCells_CorrectDimensionCount()
-        {
-            const int expected = 2;
-
-            var cellsArray = BoardManager.CreateCells(0);
-
-            cellsArray.Rank.Should().Be(expected);
-        }
-
-        [Theory]
-        [InlineData(ushort.MinValue)]
-        [InlineData(10)]
-        public void CreateCells_CorrectDimensionsSize(ushort expected)
-        {
-            var cellsArray = BoardManager.CreateCells(expected);
-
-            cellsArray.GetLength(0).Should().Be(expected);
-            cellsArray.GetLength(1).Should().Be(expected);
-        }
-
-        [Fact]
-        public void CreateCells_CorrectCellState()
-        {
-            var cellsArray = BoardManager.CreateCells(3);
-
-            using (new AssertionScope())
-            {
-                foreach (var cell in cellsArray)
-                {
-                    cell.State.Should().Be(CellType.None);
-                }
-            }
-        }
-
-        [Fact]
-        public void CreateCells_CorrectCellNumbers()
-        {
-            const ushort gridSize = 3;
-
-            var cellsArray = BoardManager.CreateCells(gridSize).Cast<Cell>().Select(x => x.Number).ToArray();
-
-            cellsArray.Should().Contain(new ushort[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 });
-        }
-
-        [Fact]
         public void TryTurn_BoardIsNull_ThrowArgumentNullException()
         {
-            _boardManager.Invoking(x => BoardManager.TryTurn(null, 0, out _))
+            _boardManager.Invoking(x => x.Turn(null, 0))
                 .Should()
                 .Throw<ArgumentNullException>();
         }
