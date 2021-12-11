22845f911fbd52f53bbaf482485a6ca2405654b0
Artem <itrimus@gmail.com>
2021-12-11 20:21:53 +0500

    57

--- front-end/tic-tac-toe/src/app/free-game/free-game.component.ts
+++ front-end/tic-tac-toe/src/app/free-game/free-game.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { GameClient } from '../generated/clients';
 import { GameModel } from '../generated/dto';
 
 @Component({
     selector: 't-free-game',
     templateUrl: './free-game.component.html',
-    styleUrls: ['./free-game.component.css']
+    styleUrls: ['./free-game.component.css'],
+    encapsulation: ViewEncapsulation.ShadowDom
 })
 export class FreeGameComponent {
 
